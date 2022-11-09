b4af224d2da128558c75e6671f5da128aa32d73b
jamalshabani <jjuma1992@gmail.com>
2022-11-09 10:38:39 -0500

    Minimum compliance with 3 materials

---
+++ run_3_mat_compliance.py
@@ -0,0 +1,16 @@
+import subprocess
+
+program_list = ["rm -rf test1", "rm -rf test2", "rm -rf test3",
+                "python3 3_mat_compliance.py -tao_type cg -tao_monitor -tao_max_it 200 -tao_gatol 1e-7 -lr 0.35 -ls 0.35 -m '1_to_6_mesh.msh' -o 'motion_nof1' -er 1.0 -es 1.0e-2 -k 1.7e-3 -e 0.5e-3 -p 2.0"]
+
+
+i = 1
+for program in program_list:
+    print("------------------------------------------------------------------------------")
+    print("")
+    print("Running test #{}".format(i))
+    print("")
+    print(program)
+    print("")
+    subprocess.run(program, shell = True)
+    i = i + 1
