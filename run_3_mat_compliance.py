488b5f66c14effcef72ef7e6d8b0029b75991e72
jamalshabani <jjuma1992@gmail.com>
2022-11-10 10:06:07 -0500

    Change epsilon

--- run_3_mat_compliance.py
+++ run_3_mat_compliance.py
@@ -1,7 +1,8 @@
 import subprocess
 
+#"python3 3_mat_compliance.py -tao_type bncg -tao_monitor -tao_max_it 200 -tao_gatol 1e-7 -m '1_to_3_mesh.msh' -o 'test1' -er 1.0 -es 1.0e-2 -vr 0.3 -vs 0.4 -k 2.2e-3 -e 1.0e-3 -p 2.0 -q 1.0"
 program_list = ["rm -rf test1", "rm -rf test2", "rm -rf test3",
-                "python3 3_mat_compliance.py -tao_type bncg -tao_monitor -tao_max_it 200 -tao_gatol 1e-7 -m '1_to_3_mesh.msh' -o 'test1' -er 1.0 -es 1.0e-2 -vr 0.3 -vs 0.4 -k 2.2e-3 -e 1.0e-3 -p 2.0 -q 1.0"]
+                "python3 3_mat_compliance.py -tao_type bncg -tao_monitor -tao_max_it 200 -tao_gatol 1e-7 -m '1_to_3_mesh.msh' -o 'test1' -er 1.0 -es 1.0e-2 -vr 0.3 -vs 0.4 -k 2.2e-5 -e 0.025 -p 2.0 -q 1.0"]
 
 
 i = 1
