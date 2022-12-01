1e0d52a42b1163dd66b29c7175450835adbece88
jamalshabani <jjuma1992@gmail.com>
2022-12-01 00:59:00 -0500

    Output files

--- run_3_mat_compliance_responsive.py
+++ run_3_mat_compliance_responsive.py
@@ -2,7 +2,7 @@
 import subprocess
 
 program_list = ["rm -rf test1", "rm -rf test2", "rm -rf test3",
-                "python3 3_mat_compliance_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 10000 -tao_ls_type more-thuente -m '1_to_3_mesh.msh' -o 'test3' -er 1.0 -es 1.0 -lr 2.0 -ls 1.2 -vr 0.3 -vs 0.3 -k 1.0e-3 -e 2.0e-3 -p 2.0 -q 1.0"]
+                "python3 3_mat_compliance_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 10000 -tao_ls_type more-thuente -m '1_to_3_mesh.msh' -o 'test3' -er 10.0 -es 10.0 -lr 2.0 -ls 0.5 -vr 0.5 -vs 0.5 -k 2.0e-3 -e 4.0e-3 -p 2.0 -q 1.0"]
 
 
 i = 1
