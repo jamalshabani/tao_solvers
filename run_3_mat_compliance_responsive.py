ae4537eb4c603565e1cca01b7449a71b4c0ba114
jamalshabani <jjuma1992@gmail.com>
2022-11-30 19:36:22 -0500

    Changes to compliance run script

--- run_3_mat_compliance_responsive.py
+++ run_3_mat_compliance_responsive.py
@@ -2,7 +2,7 @@
 import subprocess
 
 program_list = ["rm -rf test1", "rm -rf test2", "rm -rf test3",
-                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 10000 -tao_ls_type more-thuente -m 'motion_mesh2.msh' -o 'test3' -er 1.0 -es 1.0e-2 -lr 1.0 -ls 0.09 -vr 0.3 -vs 0.5 -k 2.0e-3 -e 4.0e-3 -p 2.0 -q 1.0"]
+                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 10000 -tao_ls_type more-thuente -m 'motion_mesh2.msh' -o 'test3' -er 1.0 -es 1.0 -lr 1.0 -ls 0.09 -vr 0.3 -vs 0.5 -k 2.0e-3 -e 4.0e-3 -p 2.0 -q 1.0 -s 1.0"]
 
 
 i = 1
