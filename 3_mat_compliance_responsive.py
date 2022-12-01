fd268d3cd22729db6de2419748ddabc08375f7ca
jamalshabani <jjuma1992@gmail.com>
2022-11-30 21:06:06 -0500

    Changes initial design

--- 3_mat_compliance_responsive.py
+++ 3_mat_compliance_responsive.py
@@ -62,64 +62,8 @@ s = Function(V, name = "Stimulus")
 trace = Function(V, name = "Trace tr(e(u))")
 
 x, y = SpatialCoordinate(mesh)
-
-# Define the points
-v1 = [[0.1, 1/12], [0.3, 1/12], [0.5, 1/12], [0.7, 1/12], [0.9, 1/12]]
-r1 = [[0.2, 1/12], [0.4, 1/12], [0.6, 1/12], [0.8, 1/12]]
-
-v2 = [[0.2, 2/12], [0.4, 2/12], [0.6, 2/12], [0.8, 2/12]]
-r2 = [[0.1, 2/12], [0.3, 2/12], [0.5, 2/12], [0.7, 2/12], [0.9, 2/12]]
-
-v3 = [[0.1, 3/12], [0.3, 3/12], [0.5, 3/12], [0.7, 3/12], [0.9, 3/12]]
-r3 = [[0.2, 3/12], [0.4, 3/12], [0.6, 3/12], [0.8, 3/12]]
-
-v = v1 + v2 + v3
-r = r1 + r2 + r3
-s = v + r
-
-def dist(x, y, a, b):
-	return sqrt((x - a)**2 + (y - b)**2)
-
-def g_s(s):
-	if (s < 0.02):
-		return 0
-	if (0.02 < s < 0.03):
-		return (cos((s - 0.03)*pi/0.01) + 1)/2
-	if (s > 0.03):
-		return 1
-
-def g_r(s):
-	if (s < 0.02):
-		return 1
-	if (0.02 < s < 0.03):
-		return (cos((s - 0.02)*pi/0.01) + 1)/2
-	if (s > 0.03):
-		return 0
-
-M = len(mesh_coordinates)
-rho2_array = np.ones(M)
-rho3_array = np.zeros(M)
-
-for i in range(len(s)):
-	for j in range(M):
-		x = mesh_coordinates[j][0]
-		y = mesh_coordinates[j][1]
-		temp = g_s(dist(x, y, s[i][0], s[i][1]))
-		if (temp < 1):
-			rho2_array[j] = temp
-
-for i in range(len(r)):
-	for j in range(M):
-		x = mesh_coordinates[j][0]
-		y = mesh_coordinates[j][1]
-		temp = g_r(dist(x, y, r[i][0], r[i][1]))
-		if (temp > 0):
-			rho3_array[j] = temp
-
-rho2.dat.data[:] = rho2_array
-rho3.dat.data[:] = rho3_array
-# rho2.interpolate(Constant(options.volume_s))
-# rho3.interpolate(Constant(options.volume_r))
+rho2.interpolate(Constant(options.volume_s))
+rho3.interpolate(Constant(options.volume_r))
 s.interpolate(Constant(options.steamy))
 
 rho = as_vector([rho2, rho3, s])
