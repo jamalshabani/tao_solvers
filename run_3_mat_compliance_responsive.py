d9b691bad934d8a17c8979133fdb20114603a997
jamalshabani <jjuma1992@gmail.com>
2022-12-01 23:40:17 -0500

    Saved scripts

--- run_3_mat_compliance_responsive.py
+++ run_3_mat_compliance_responsive.py
@@ -2,7 +2,12 @@
 import subprocess
 
 program_list = ["rm -rf test1", "rm -rf test2", "rm -rf test3",
-                "python3 3_mat_compliance_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 10000 -tao_ls_type more-thuente -m '1_to_3_mesh.msh' -o 'test3' -er 10.0 -es 10.0 -lr 1.5 -ls 0.5 -vr 0.5 -vs 0.5 -k 2.0e-3 -e 4.0e-3 -p 2.0 -q 1.0"]
+                "python3 3_mat_compliance_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type armijo -m '1_to_3_mesh.msh' -o 'test1' -er 30e-1 -es 30e-1 -lr 2.0 -ls 1.0 -vr 0.4 -vs 0.4 -k 1.0e-3 -e 4.0e-3 -p 2.0 -q 1.0",
+                "python3 3_mat_compliance_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type armijo -m '1_to_3_mesh.msh' -o 'test2' -er 40e-1 -es 40e-1 -lr 2.0 -ls 1.0 -vr 0.4 -vs 0.4 -k 1.0e-3 -e 4.0e-3 -p 2.0 -q 1.0",
+                "python3 3_mat_compliance_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type armijo -m '1_to_3_mesh.msh' -o 'test3' -er 50e-1 -es 50e-1 -lr 2.0 -ls 1.0 -vr 0.4 -vs 0.4 -k 1.0e-3 -e 4.0e-3 -p 2.0 -q 1.0",
+                "python3 3_mat_compliance_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type armijo -m '1_to_3_mesh.msh' -o 'test4' -er 60e-1 -es 60e-1 -lr 2.0 -ls 1.0 -vr 0.4 -vs 0.4 -k 1.0e-3 -e 4.0e-3 -p 2.0 -q 1.0",
+                "python3 3_mat_compliance_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type armijo -m '1_to_3_mesh.msh' -o 'test5' -er 70e-1 -es 70e-1 -lr 2.0 -ls 1.0 -vr 0.4 -vs 0.4 -k 1.0e-3 -e 4.0e-3 -p 2.0 -q 1.0"]
+
 
 
 i = 1
@@ -15,3 +20,11 @@ for program in program_list:
     print("")
     subprocess.run(program, shell = True)
     i = i + 1
+
+
+
+
+# Scripts for ratio E_r/E_s = 1.0
+#"python3 3_mat_compliance_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type armijo -m '1_to_3_mesh.msh' -o 'test2' -er 40e-1 -es 40e-1 -lr 2.0 -ls 1.0 -vr 0.4 -vs 0.4 -k 1.0e-3 -e 4.0e-3 -p 2.0 -q 1.0",
+#"python3 3_mat_compliance_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type armijo -m '1_to_3_mesh.msh' -o 'test3' -er 50e-1 -es 50e-1 -lr 2.0 -ls 1.0 -vr 0.4 -vs 0.4 -k 1.0e-3 -e 4.0e-3 -p 2.0 -q 1.0",
+#"python3 3_mat_compliance_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type armijo -m '1_to_3_mesh.msh' -o 'test4' -er 60e-1 -es 60e-1 -lr 2.0 -ls 1.0 -vr 0.4 -vs 0.4 -k 1.0e-3 -e 4.0e-3 -p 2.0 -q 1.0",
