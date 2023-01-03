60622dc0137089a02c5a610030c49a87e757fc65
jamalshabani <jjuma1992@gmail.com>
2023-01-03 16:26:29 -0500

    Changes

--- run_3_mat_least_square_responsive.py
+++ run_3_mat_least_square_responsive.py
@@ -1,9 +1,9 @@
 import subprocess
 
 program_list = ["rm -rf test1", "rm -rf test2", "rm -rf test3", "rm -rf test4",
-                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type unit -m 'motion_mesh2.msh' -o 'test1' -er 100 -es 1.0 -lr 0.08 -ls 0.001 -vr 1.0 -vs 1.0 -k 5.0e-5 -e 4.0e-3 -p 2.0 -q 1.0",
-                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type armijo -m 'motion_mesh2.msh' -o 'test2' -er 100 -es 1.0 -lr 0.08 -ls 0.001 -vr 1.0 -vs 1.0 -k 5.0e-5 -e 4.0e-3 -p 2.0 -q 1.0",
-                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type more-thuente -m 'motion_mesh2.msh' -o 'test3' -er 100 -es 1.0 -lr 0.08 -ls 0.001 -vr 1.0 -vs 1.0 -k 5.0e-5 -e 4.0e-3 -p 2.0 -q 1.0"]
+                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type unit -m 'motion_mesh2.msh' -o 'test1' -er 100 -es 1.0 -lr 0.1 -ls -0.001 -vr 0.5 -vs 0.5 -k 5.0e-5 -e 1.0e-3 -p 2.0 -q 1.0",
+                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type armijo -m 'motion_mesh2.msh' -o 'test2' -er 100 -es 1.0 -lr 0.1 -ls -0.001 -vr 0.5 -vs 0.5 -k 5.0e-5 -e 1.0e-3 -p 2.0 -q 1.0",
+                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 100 -tao_ls_type more-thuente -m 'motion_mesh2.msh' -o 'test3' -er 100 -es 1.0 -lr 0.1 -ls -0.001 -vr 0.5 -vs 0.5 -k 5.0e-5 -e 1.0e-3 -p 2.0 -q 1.0"]
 
 
 
