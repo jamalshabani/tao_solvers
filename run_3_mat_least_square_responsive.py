bffb8731a3e44ba64916d6c176b2e3e3b476d36a
jamalshabani <jjuma1992@gmail.com>
2023-05-05 10:20:06 -0400

    “Changes”

--- run_3_mat_least_square_responsive.py
+++ run_3_mat_least_square_responsive.py
@@ -1,9 +1,9 @@
 import subprocess
 
 program_list = ["rm -rf test1", "rm -rf test2", "rm -rf test3",
-                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 5000 -tao_ls_type more-thuente -m 'motion_mesh.msh' -o 'test1' -er 1.0 -es 1.0e-2 -lr 2.0 -ls 0.25 -vr 0.3 -vs 0.5 -k 5.0e-3 -e 4.0e-3 -p 2.0 -q 1.0 -s 1",
-                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 5000 -tao_ls_type armijo -m 'motion_mesh.msh' -o 'test2' -er 1.0 -es 1.0e-2 -lr 2.0 -ls 0.25 -vr 0.3 -vs 0.5 -k 5.0e-3 -e 4.0e-3 -p 2.0 -q 1.0 -s 1",
-                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 5000 -tao_ls_type unit -m 'motion_mesh.msh' -o 'test3' -er 1.0 -es 1.0e-2 -lr 2.0 -ls 0.25 -vr 0.3 -vs 0.5 -k 5.0e-3 -e 4.0e-3 -p 2.0 -q 1.0 -s 1"]
+                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 1000 -tao_ls_type more-thuente -m 'motion_mesh.msh' -o 'test1' -er 1.0 -es 1.0e-2 -lr 2.0 -ls 0.25 -vr 0.3 -vs 0.5 -k 5.0e-3 -e 4.0e-3 -p 2.0 -q 1.0 -s 1",
+                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 1000 -tao_ls_type armijo -m 'motion_mesh.msh' -o 'test2' -er 1.0 -es 1.0e-2 -lr 2.0 -ls 0.25 -vr 0.3 -vs 0.5 -k 5.0e-3 -e 4.0e-3 -p 2.0 -q 1.0 -s 1",
+                "python3 3_mat_least_square_responsive.py -tao_type bncg -tao_max_funcs 10000 -tao_gatol 1.0e-7 -tao_grtol 1.0e-7 -tao_gttol 1.0e-7 -tao_converged_reason -tao_monitor -tao_max_it 1000 -tao_ls_type unit -m 'motion_mesh.msh' -o 'test3' -er 1.0 -es 1.0e-2 -lr 2.0 -ls 0.25 -vr 0.3 -vs 0.5 -k 5.0e-3 -e 4.0e-3 -p 2.0 -q 1.0 -s 1"]
 
 
 
